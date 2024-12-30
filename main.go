221ac73af353636702bb492a626a50d26f6ce9b6
OhNishant <nishant.mishra550@gmail.com>
2024-12-30 19:57:58 +0530

    feat: REPL, just a basic one

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/ohnishat/peepoScript/cmd/repl"
+)
+
+const PEEPO_SCRIPT_VERSION = "0.1.0"
 
 func main() {
-	fmt.Println("Hello, peepo")
+	fmt.Printf("Peepo Script - %s | REPL\n\n\n", PEEPO_SCRIPT_VERSION)
+	repl.StartRepl(os.Stdin, os.Stdout)
 }
